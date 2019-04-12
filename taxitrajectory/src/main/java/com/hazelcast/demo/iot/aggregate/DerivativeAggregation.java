d7855409545f82a9cd49ffabccae009c08d6f08a
Manuel David <manny.david@hazelcast.com>
2019-04-12 08:50:54 +0300

    Accumulate version 1

--- taxitrajectory/src/main/java/com/hazelcast/demo/iot/aggregate/DerivativeAggregation.java
+++ taxitrajectory/src/main/java/com/hazelcast/demo/iot/aggregate/DerivativeAggregation.java
@@ -1,10 +1,13 @@
 package com.hazelcast.demo.iot.aggregate;
 
+import com.hazelcast.demo.iot.data.TaxiData;
 import com.hazelcast.jet.aggregate.AggregateOperation;
 import com.hazelcast.jet.aggregate.AggregateOperation1;
 import com.hazelcast.jet.function.BiFunctionEx;
 
 import java.io.Serializable;
+import java.util.Map;
+
 
 public class DerivativeAggregation {
 
@@ -21,14 +24,30 @@ public class DerivativeAggregation {
                     if (acc.lastItem == null) {
                         acc.lastItem = item;
                     }
+
                     acc.lastDifference = differenceFn.apply(item, acc.lastItem);
+                    acc.totalDifference = acc.totalDifference + ((Double) acc.lastDifference);
+
+                    TaxiData lastTaxiData = (TaxiData) acc.lastItem;
+                    TaxiData thisTaxiData = (TaxiData) item;
+
+                    long deltaMsec = thisTaxiData.getTimestamp( ).getTime( ) -
+                            lastTaxiData.getTimestamp( ).getTime( );
+
+                    double deltaHours = ((double) deltaMsec) / (1000 * 60 * 60);
+
+                    acc.speed = ((Double) acc.lastDifference) / deltaHours;
+
+
                     acc.lastItem = item;
                 })
-                .andExportFinish(acc -> acc.lastDifference);
+                .andExportFinish(acc -> (OUT) acc.speed);
     }
 
     private static final class DerivativeAccumulator<T, OUT> implements Serializable {
         T lastItem;
         OUT lastDifference;
+        Double totalDifference = 0.0;
+        Double speed = 0.0;
     }
 }
